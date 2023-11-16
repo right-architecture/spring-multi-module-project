7c7d56fbb4209185565fcc4a9ad17136a3200b1f
Gyuwon Yi <gyuwon@live.com>
2023-11-16 18:37:18 +0900

    ProductRepository 인터페이스 메서드 정의

--- bounded-context/inventory/domain-model/src/main/java/commerce/inventory/ProductRepository.java
+++ bounded-context/inventory/domain-model/src/main/java/commerce/inventory/ProductRepository.java
@@ -1,4 +1,14 @@
 package commerce.inventory;
 
+import java.util.UUID;
+import java.util.function.Consumer;
+
 public interface ProductRepository {
+
+    void create(Product product);
+
+    void update(
+        UUID id,
+        Consumer<Product> updater,
+        Consumer<UUID> notFoundHandler);
 }
